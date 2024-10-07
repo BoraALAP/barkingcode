068c4f90e393e99ff087391f5a33272a0c460b94
Bora ALAP <bora.alap@artticfox.com>
2024-10-07 02:02:06 +0200

    remove .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/artticfox/Desktop/Work/barkingcode/src/app/layout\":[\"static/media/84455f2b5a591033-s.p.woff2\",\"static/media/e1c529c04de64b40-s.p.woff2\",\"static/media/463dafcda517f24f-s.p.woff\",\"static/media/4473ecc91f70f139-s.p.woff\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
